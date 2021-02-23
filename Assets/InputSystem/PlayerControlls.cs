b0178aef9829b6189e478c821739f7468779d963
Fredrik Storehaug <fstorehaug@hotmail.com>
2021-02-23 08:35:44 +0100

    added input system with ultrageneric input action set

---
+++ Assets/InputSystem/PlayerControlls.cs
@@ -0,0 +1,231 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputSystem/PlayerControlls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControlls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControlls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControlls"",
+    ""maps"": [
+        {
+            ""name"": ""KeyboardMouse"",
+            ""id"": ""98219086-52ad-43a1-b090-e9aca36fb051"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""3c7c6960-9c30-4fe8-8801-09b1ca5f8fdf"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""b101ece2-5aa7-4696-8e45-57763d3688ca"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""0623978e-d735-4c8b-9a98-03b122995f75"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""2e4867de-362c-42cb-a908-8af97e23e0a5"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""e0954129-374a-49e9-94dc-642eba821542"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""c0903d12-27d5-4565-a529-905c9e774ec2"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""ArrowKeys"",
+                    ""id"": ""cdfab9a6-431f-4e10-afa3-0e62d20a9a22"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""fdf7266d-316b-442b-b984-b2b81115522c"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""482b69ba-1239-4d26-914a-c233d90e0500"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""82fe17da-1589-4455-8f84-54df3f04d9da"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""90eefdc8-47c3-4bce-bb6a-2acf7a266afd"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // KeyboardMouse
+        m_KeyboardMouse = asset.FindActionMap("KeyboardMouse", throwIfNotFound: true);
+        m_KeyboardMouse_Move = m_KeyboardMouse.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // KeyboardMouse
+    private readonly InputActionMap m_KeyboardMouse;
+    private IKeyboardMouseActions m_KeyboardMouseActionsCallbackInterface;
+    private readonly InputAction m_KeyboardMouse_Move;
+    public struct KeyboardMouseActions
+    {
+        private @PlayerControlls m_Wrapper;
+        public KeyboardMouseActions(@PlayerControlls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_KeyboardMouse_Move;
+        public InputActionMap Get() { return m_Wrapper.m_KeyboardMouse; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(KeyboardMouseActions set) { return set.Get(); }
+        public void SetCallbacks(IKeyboardMouseActions instance)
+        {
+            if (m_Wrapper.m_KeyboardMouseActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_KeyboardMouseActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_KeyboardMouseActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_KeyboardMouseActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_KeyboardMouseActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public KeyboardMouseActions @KeyboardMouse => new KeyboardMouseActions(this);
+    public interface IKeyboardMouseActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
