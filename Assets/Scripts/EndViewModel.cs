a4135e4bb33e99a99d0da25fbccbd1210b87befa
Mathias Solheim <func.mathias@gmail.com>
2021-02-23 20:02:07 +0100

    Added ui and viewmodel to end sceene

---
+++ Assets/Scripts/EndViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityWeld.Binding;
+using UnityEngine.SceneManagement;
+
+[Binding]
+public class EndViewModel : MonoBehaviour
+{
+    public SettingsScriptableObject settings;
+
+    [Binding]
+    public int playerScore
+    {
+        get
+        {
+            return settings.playerScore;
+        }
+    }
+
+    [Binding]
+    public void BackToStart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
