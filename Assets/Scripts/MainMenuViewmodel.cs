9f1dbfdaceb360cf23f47f88405951d19784dfef
Fredrik Storehaug <fstorehaug@hotmail.com>
2021-02-23 15:26:13 +0100

    added sceenes for main menu and end sceene

---
+++ Assets/Scripts/MainMenuViewmodel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityWeld.Binding;
+using UnityEngine.SceneManagement;
+
+public class MainMenuViewmodel : MonoBehaviour
+{
+    [Binding]
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByName("SampleScene").buildIndex);
+    }
+
+}
